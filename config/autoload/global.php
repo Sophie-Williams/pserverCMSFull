cc2def3ff018f86ffe4d38b1706cc0fb00baa394
kokspflanze <kokspflanze@gmx.de>
2014-07-03 00:18:25 +0200

    init

---
+++ config/autoload/global.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * Global Configuration Override
+ *
+ * You can use this file for overriding configuration values from modules, etc.
+ * You would place values in here that are agnostic to the environment and not
+ * sensitive to security.
+ *
+ * @NOTE: In practice, this file will typically be INCLUDED in your source
+ * control, so do not include passwords or other sensitive information in this
+ * file.
+ */
+
+return array(
+	'zenddevelopertools' => array(
+		/**
+		 * General Profiler settings
+		 */
+		'profiler' => array(
+			'enabled' => false,
+			'strict' => true,
+			'flush_early' => true,
+			'cache_dir' => 'data/cache',
+			'matcher' => array(),
+			'collectors' => array(),
+		),
+		/**
+		 * General Toolbar settings
+		 */
+		'toolbar' => array(
+			'enabled' => false,
+			'auto_hide' => true,
+			'position' => 'bottom',
+			'version_check' => true,
+			'entries' => array(),
+		),
+	),
+);
