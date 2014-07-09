a6efe7763b66f0126cb0c67320c0dde4783164ab
kokspflanze <kokspflanze@gmx.de>
2014-07-09 02:28:59 +0200

    dataservice integration

--- public/test.php
+++
@@ -1,8 +0,0 @@
-<?php
-$DB_TYPE = 'dblib'; //Type of database<br>
-$DB_HOST = '80.86.81.16:5465'; //Host name<br>
-$DB_USER = 'sa'; //Host Username<br>
-$DB_PASS = 'Betze1989'; //Host Password<br>
-$DB_NAME = 'SRO_VT_ACCOUNT'; //Database name<br><br>
-
-$dbh = new PDO("$DB_TYPE:host=$DB_HOST; dbname=$DB_NAME;", $DB_USER, $DB_PASS);
