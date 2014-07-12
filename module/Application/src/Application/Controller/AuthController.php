dc91a645675b15aaf4bae5f38da44e6c80d31112
kokspflanze <kokspflanze@gmx.de>
2014-07-13 00:45:46 +0200

    auth + twig

---
+++ module/Application/src/Application/Controller/AuthController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Application\Controller;
+
+use Zend\Mvc\Controller\AbstractActionController;
+use Zend\View\Model\ViewModel;
+
+class AuthController extends AbstractActionController {
+	public function indexAction() {
+
+		return new ViewModel();
+	}
+}
