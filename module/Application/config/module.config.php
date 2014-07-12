10dd2f9277bdfbbbd049433dcac78a81df0c82ec
kokspflanze <kokspflanze@gmx.de>
2014-07-12 21:53:16 +0200

    extra für pupsbacke

--- module/Application/config/module.config.php
+++ module/Application/config/module.config.php
@@ -89,14 +89,14 @@ return array(
 
 	'doctrine' => array(
 		'driver' => array(
-			'zfcuser_entity' => array(
+			'application_entities' => array(
 				'class' =>'Doctrine\ORM\Mapping\Driver\AnnotationDriver',
 				'cache' => 'array',
 				'paths' => array(__DIR__ . '/../src/Application/Entity')
 			),
 			'orm_default' => array(
 				'drivers' => array(
-					'Application\Entity' => 'zfcuser_entity'
+					'Application\Entity' => 'application_entities'
 				),
 			),
 		),
