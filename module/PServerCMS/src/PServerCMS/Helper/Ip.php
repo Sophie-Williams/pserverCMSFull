e0a775aa3428dd9b90a96ff418942cc52464be63
kokspflanze <kokspflanze@gmx.de>
2014-10-08 00:02:48 +0200

    use Helper\Ip from PaymentApi

--- module/PServerCMS/src/PServerCMS/Helper/Ip.php
+++ module/PServerCMS/src/PServerCMS/Helper/Ip.php
@@ -6,38 +6,7 @@ namespace PServerCMS\Helper;
  * Class Ip
  * @package PServerCMS\Helper
  */
-class Ip{
-
-    /**
-     * @var string cache ClientIP
-     */
-    private static $sIP;
-
-	/**
-	 * @return string
-	 */
-	public static function getIp(){
-
-        if(self::$sIP){
-            return self::$sIP;
-        }
-
-		if(isset($_SERVER['HTTP_X_FORWARDED_FOR']) && !empty($_SERVER['HTTP_X_FORWARDED_FOR'])){
-			$sResult = $_SERVER['HTTP_X_FORWARDED_FOR'];
-		}else{
-			$sResult = $_SERVER['REMOTE_ADDR'];
-		}
-
-		// Check if there smth like xxx.xxx.xxx.xxx[internal-network-ip], xxx.xxx.xxx.xxx[www-network-ip]
-		if(strpos($sResult, ',') === true){
-			$aResult = explode(',', $sResult);
-			$sResult = trim($aResult[count($aResult)-1]);
-		}
-
-        self::$sIP = $sResult;
-
-		return $sResult;
-	}
+class Ip extends \PaymentAPI\Helper\Ip {
 
 	/**
 	 * @param $sIp
