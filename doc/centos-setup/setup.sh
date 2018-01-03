0454b02b10d4e5618fdbe43fefc64a15d19d7eb5
KoKsPfLaNzE <kokspflanze@gmx.de>
2018-01-03 21:29:22 +0100

    update guide for php72

--- doc/centos-setup/setup.sh
+++ doc/centos-setup/setup.sh
@@ -8,13 +8,17 @@ done
 firewall-cmd --reload
 
 rpm -Uvh https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
-rpm -Uvh https://mirror.webtatic.com/yum/el7/webtatic-release.rpm
+rpm -Uvh https://rpms.remirepo.net/enterprise/remi-release-7.rpm
+
+yum -y install yum-utils
+yum-config-manager --enable remi-php72
 
 # stay up to date
 yum -y update
 
 # php && httpd
-yum -y install mod_php71w php71w-opcache php71w-cli php71w-common php71w-gd php71w-intl php71w-mbstring php71w-mcrypt php71w-mysql php71w-mssql php71w-pdo php71w-pear php71w-xml php71w-xmlrpc httpd
+yum -y install php72 php72-php php72-php-opcache php72-php-bcmath php72-php-cli php72-php-common php72-php-gd php72-php-intl php72-php-json php72-php-mbstring php72-php-pdo php72-php-pdo-dblib php72-php-pear php72-php-pecl-mcrypt php72-php-xmlrpc php72-php-xml php72-php-mysql php72-php-soap php72-php-pecl-zip httpd
+
 
 # helpful tools
 yum -y install epel-release curl crontabs git
@@ -22,6 +26,8 @@ yum -y install epel-release curl crontabs git
 systemctl enable httpd crond
 
 echo "expose_php = Off" > /etc/php.d/custome.ini
+
+ln -s /bin/php72 /bin/php
 
 echo "ServerTokens prod
 ServerSignature Off
