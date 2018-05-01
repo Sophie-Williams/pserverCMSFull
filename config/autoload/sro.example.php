ff21b46034120f7628ad961216e21a5d12fa22a6
KoKsPfLaNzE <kokspflanze@gmx.de>
2018-05-01 18:25:38 +0200

    bs4 update fontawesome 5 update

--- config/autoload/sro.example.php
+++ config/autoload/sro.example.php
@@ -76,7 +76,7 @@ return [
                     0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23
                 ],
                 'min' => 30,
-                'icon' => 'fa fa-cubes'
+                'icon' => 'fas fa-cubes',
             ],
             [
                 'name' => 'Medusa',
@@ -84,23 +84,24 @@ return [
                     1,22,23
                 ],
                 'min' => 14,
-                'icon' => 'fa fa-digg'
+                'icon' => 'fas fa-crosshairs',
             ],
             //'Sunday' | 'Monday' | 'Tuesday' | 'Wednesday' | 'Thursday' | 'Friday' | 'Saturday'
             [
                 'name' => 'Fortresswar',
                 'days' => [
-                    'Wednesday','Monday'
+                    'Wednesday',
+                    'Monday',
                 ],
                 'hour' => 8,
                 'min' => 14,
-                'icon' => 'fa fa-bomb'
+                'icon' => 'fas fa-warehouse',
             ],
             [
                 'name' => 'Register',
                 'type' => 'static',
                 'time' => 'Saturday 12:00 - 23:00',
-                'icon' => 'fa fa-digg'
+                'icon' => 'fas fa-hourglass-start',
             ],
         ],
     ],
